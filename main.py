9adc5ab7056f41dd649d13a49a5b00383d8865d5
Edcactus2 <158601650+Edcactus2@users.noreply.github.com>
2024-02-02 18:11:58 +0000

    routes

--- main.py
+++ main.py
@@ -6,5 +6,24 @@ app = Flask("test")
 def index():
   return ("coucou")
 
+@app.route('/register')
+def register():
+  return render_template("register.html")
+
+@app.route('/contact')
+def contact():
+  return render_template("contact.html")
+
+@app.route('/sneakers')
+def sneakers():
+  return render_template("sneakers.html")
+
+@app.route('/phones')
+def phones():
+  return render_template("phones.html")
+
+@app.route('/clothes')
+def clothes():
+  return render_template("clothes.html")
 
 app.run(host="0.0.0.0", port=3904)
